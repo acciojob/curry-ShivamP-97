17324ce43b4fefba5fc06c5e8cce1b37fcdaeea6
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-20 14:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function curry(callback) {
-  
+	const collected=[];
+	
+	function curried(...args) {
+		if(args.length===0){
+			const result=callback(...collected);
+			collected.length=0;
+			return result;
+		}
+
+		collected.push(...args);
+		return curried;
+	}
+
+	return curried;
 }
 
 module.export=curry
